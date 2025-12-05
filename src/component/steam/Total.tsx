d0261423c8fd2e1daf0fd3a7b1dd26897efd913f
Selbi <selbibegiyeva1@gmail.com>
2025-12-05 13:02:00 +0500

    bank modal window

--- src/component/steam/Total.tsx
+++ src/component/steam/Total.tsx
@@ -1,4 +1,8 @@
-function Total() {
+interface ModalProps {
+    click: () => void;
+}
+
+function Total({ click }: ModalProps) {
     return (
         <div className="px-5 py-8 bg-[#1D1D22] rounded-4xl">
             <b className="text-[20px]">Оплата</b>
@@ -14,7 +18,7 @@ function Total() {
                 </div>
             </div>
 
-            <button className="bg-[#A132C7] p-[11.5px] w-full rounded-[10px] text-[14px] font-bold">Оплатить</button>
+            <button onClick={click} className="bg-[#A132C7] p-[11.5px] w-full rounded-[10px] text-[14px] font-bold">Оплатить</button>
         </div>
     )
 }
